be5699148d4dadefb42802247d1f43cee45c1a71
Justen <justen27@live.com>
2023-05-28 18:50:12 -0700

    got api routes working

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const fs = require("fs");
 const path = require("path");
+const api = require("./routes/index.js");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use("/api", api);
 
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
